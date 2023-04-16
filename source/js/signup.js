51779d7a3a5496c4bc85cc49e334516327ae6e65
gardenia06 <harrietlo92@gmail.com>
2023-04-16 20:58:23 +0800

    check email input

--- source/js/signup.js
+++ source/js/signup.js
@@ -1,6 +1,8 @@
 //Here we handle all signup algorithm
 
 //Variables from the DOM
+var email = document.querySelector(".in_email");
+
 var button_back = document.querySelector(".back_btn");
 var button_sendemail = document.querySelector(".sendemail_btn");
 
@@ -9,11 +11,25 @@ button_back.addEventListener("click",function(event){
 });
 
 button_sendemail.addEventListener("click",function(event){
-	signup();
+	if (checkEmail(email)) signup();
 });
 
-function signup(){
-	//send confirmation email 
+function checkEmail(email){
+	var re= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (email.value.length == 0){
+		alert("Please enter your email.");
+		return false;
+	} else if (re.test(email.value)) {
+		alert("Email sent! Please check your mailbox.");
+		return true;
+	}
+	alert("Please check your email format.");
+	return false;
+}
+
+function signup(email){
+
+
 }
 
 
